fb23601817374003fc8b3c304f1e3c33ea67fc72
mksbnt <mksbnt@hotmail.com>
2020-03-28 04:28:31 +0200

    First class component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import './App.css';
 
-function MovieItem(props) {
+const movie = {
+  title: "Hesher",
+  rate: 8.1,
+  image: "https://lh3.googleusercontent.com/proxy/RZW-DK87fvRzHuJMGCbBdkvd06VXxT3nZVf_4IPU7jqmrFNy9pKjreXuqS1P8zCWw2UDyaWbEAEPDUfLnWK3VKDy0lonIfkqK6c7uBfJUKRfQaE",
+  overview: "Hesher is a 2010 American dark comedy/drama film co-written and directed by Spencer Susser and starring Joseph Gordon-Levitt, Rainn Wilson, Natalie Portman (who also produced the film) and Devin Brochu.",
+}
+
+function Image(props) {
   return (
-    <div>
-      <p>{props.title}</p>
-      <p>{props.rate}</p>
-    </div>
-  );
+    <img src={props.src} alt={props.alt}/>
+  )
+}
+
+class MovieItem extends React.Component {
+  render() {
+    const {data: {title, rate, image}} = this.props;
+    return (
+      <div>
+        <Image src={image} alt={title}/>
+        <p>{title}</p>
+        <p>{rate}</p>
+      </div>
+    )
+  }
 }
 
 function App() {
   return (
     <div>
-      <MovieItem title="New Title" rate={8.1}/>
+      <MovieItem data={movie}/>
     </div>
   );
 }
