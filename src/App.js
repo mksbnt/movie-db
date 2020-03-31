b3b96b78ab9269ac3e995f73d0228e40db53ff40
maksimk <maksimk@tallium.com>
2020-03-31 14:56:05 +0300

    Layout for cards

--- src/App.js
+++ src/App.js
@@ -1,41 +1,61 @@
 import React from "react";
 import "./App.css";
-import { moviesData } from "./moviesData";
+import {moviesData} from "./moviesData";
 import MovieItem from "./components/MovieItem";
 
 class App extends React.Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      movies: moviesData
+        this.state = {
+            movies: moviesData,
+            moviesWillWatch: []
+        };
+    }
+
+    removeMovie = movie => {
+        const updateMovies = this.state.movies.filter(function (item) {
+            return item.id !== movie.id;
+        });
+        this.setState({
+            movies: updateMovies
+        });
     };
-  }
 
-  removeMovie = movie => {
-    const updateMovies = this.state.movies.filter(function(item) {
-      return item.id !== movie.id;
-    });
-    this.setState({
-      movies: updateMovies
-    });
-  };
+    addMovieToWillWatch = movie => {
+        const updateMoviesWillWatch = [...this.state.moviesWillWatch, movie];
+        this.setState({
+            moviesWillWatch: updateMoviesWillWatch
+        });
+    };
 
-  render() {
-    return (
-      <div>
-        {this.state.movies.map(movie => {
-          return (
-            <MovieItem
-              key={movie.id}
-              movie={movie}
-              removeMovie={this.removeMovie}
-            />
-          );
-        })}
-      </div>
-    );
-  }
+    render() {
+        return (
+            <section className="section">
+                <div className="container">
+                    <div className="columns">
+                        <div className="column is-three-quarters">
+                            <div className="columns">
+                                {this.state.movies.map(movie => {
+                                    return (
+                                        <MovieItem
+                                            key={movie.id}
+                                            movie={movie}
+                                            removeMovie={this.removeMovie}
+                                            addMovieToWillWatch={this.addMovieToWillWatch}
+                                        />
+                                    );
+                                })}
+                            </div>
+                        </div>
+                        <div className="column is-one-quarter">
+                            Will watch: {this.state.moviesWillWatch.length}
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
 }
 
 export default App;
--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,14 +1,30 @@
 import React from "react";
 
 const MovieItem = props => {
-  return (
-    <div key={props.movie.id}>
-      <p>{props.movie.title}</p>
-      <button onClick={props.removeMovie.bind(this, props.movie)}>
-        Delete movie
-      </button>
-    </div>
-  );
+    const {movie, removeMovie, addMovieToWillWatch} = props;
+    return (
+        <div className="column">
+            <div key={movie.id} className="card">
+                <figure className="image is-4by3">
+                    <img className="card-image"
+                         src="https://bulma.io/images/placeholders/1280x960.png" />
+                </figure>
+                <div className="card-content">
+                    <p className="title">{movie.title}</p>
+                    <p className="subtitle">@johnsmith</p>
+                </div>
+                <footer className="card-footer">
+                    <a className="card-footer-item"
+                       onClick={addMovieToWillWatch.bind(this, movie)}
+                    >Will watch</a>
+                    <a className="card-footer-item"
+                       onClick={removeMovie.bind(this, movie)}
+                    >Delete</a>
+                </footer>
+            </div>
+        </div>
+
+    );
 };
 
 export default MovieItem;
